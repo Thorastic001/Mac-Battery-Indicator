d87fc44518a83d3ecaeaa31137dc825daae255bf
Rupayan <rupayan@Rups-MacBook-Pro-M4-16.local>
2026-05-26 00:16:22 +0530

    Fix menu bar hit-testing by adding allowsHitTesting(false) to MenuContainer

--- BatteryBoi/Views/Component/BBMenuView.swift
+++ BatteryBoi/Views/Component/BBMenuView.swift
@@ -378,7 +378,7 @@ struct MenuContainer: View {
             .environmentObject(BatteryManager.shared)
             .environmentObject(StatsManager.shared)
             .environmentObject(UpdateManager.shared)
-
+            .allowsHitTesting(false)
     }
     
 }
